77e377c876411de73b2a509c727f3c75647539fb
alvachaitanya@gmail.com <alvachaitanya@gmail.com>
2021-08-18 15:58:30 +0000

    ready for prod

--- models/staging/jaffle_shop/stg_payments.sql
+++ models/staging/jaffle_shop/stg_payments.sql
@@ -1,8 +1,9 @@
-select 
+select
     id as payment_id,
     orderid as order_id,
     paymentmethod as payment_method,
     status,
-    amount/100 as amount,
+    -- amount is stored in cents, convert it to dollars
+    {{ cents_to_dollars('amount', 4) }} as amount,
     created as created_at
-    from {{source('stripe', 'payment')}}
+from {{ source('stripe','payment') }}
